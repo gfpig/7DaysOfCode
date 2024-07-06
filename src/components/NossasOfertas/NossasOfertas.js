7052296c1d6bd123889ee8dbae84565691c745ad
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2024-07-06 19:25:08 -0300

    [DIA 04] Pegando as plantas em oferta de uma API externa

--- src/components/NossasOfertas/NossasOfertas.js
+++ src/components/NossasOfertas/NossasOfertas.js
@@ -1,48 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NossasOfertasWrapper, Titulo, Subtitulo, ListaProdutos, Item, FotoPlanta, NomePlanta, Preco, Botao } from './styled.tsx'
 
 function NossasOfertas() {
+
+    const [plantas, setPlantas] = useState([])
+
+    useEffect (() => {
+        fetchPlantas();
+    }, [])
+
+    const fetchPlantas = () => {
+        const url = "https://gist.githubusercontent.com/bugan/41d60ffa23fa0c4044cc138bf670780d/raw";
+
+        fetch(url).then(res => res.json()).then(data => {
+            //console.log(data)
+            if (data) {
+                setPlantas(data)
+            } else {
+                console.log("resposta:", data);
+            }
+        })
+    }
+
   return (
     <NossasOfertasWrapper>
         <Titulo>Conheça nossas</Titulo>
         <Subtitulo>Ofertas</Subtitulo>
 
         <ListaProdutos>
-            <Item>
-                <FotoPlanta alt="Ajuga reptans" src="images/produto-01.png" />
-                <div>
-                    <NomePlanta>Ajuga reptans</NomePlanta>
-                    <Preco>R$20,00</Preco>
-                    <Botao>
-                        Comprar
-                        <img alt="Seta para a direita" src="images/seta.svg" />
-                    </Botao>
-                </div>
-            </Item>
-            <Item>
-                <FotoPlanta alt="Cordyline fruticosa" src="images/produto-02.png" />
-                <div>
-                    <NomePlanta>Cordyline fruticosa</NomePlanta>
-                    <Preco>R$20,00</Preco>
-                    <Botao>
-                        Comprar
-                        <img alt="Seta para a direita" src="images/seta.svg" />
-                    </Botao>
-                </div>
-            </Item>
-            <Item>
-                <div className='produtos__item__imagem'>
-                    <FotoPlanta alt="Crassula ovata" src="images/produto-03.png" />
-                </div>
-                <div>
-                    <NomePlanta>Crassula ovata</NomePlanta>
-                    <Preco>R$20,00</Preco>
-                    <Botao>
-                        Comprar
-                        <img alt="Seta para a direita" src="images/seta.svg" />
-                    </Botao>
-                </div>
-            </Item>
+            {plantas.map((planta) => (
+                <Item>
+                    <FotoPlanta alt={planta.name} src={`/images/${planta.img}.png`} />
+                    <div>
+                        <NomePlanta>{planta.name}</NomePlanta>
+                        <Preco>R${planta.preco.toPrecision(4)}.</Preco>
+                        <Botao>
+                            Comprar
+                            <img alt="Seta para a direita" src="images/seta.svg" />
+                        </Botao>
+                    </div>
+                </Item>
+            ))}
             <Item>
                 <div className='produtos__item__imagem'>
                     <FotoPlanta alt="Cyperus rotundus" src="images/produto-04.png" style={{transform:"rotate(45deg)"}} />
